065da6048843183c1192ab6b25f96907c6f9d6e1
mbelfkih0612-rgb <mbelfkih0612@gmail.com>
2026-03-17 05:07:05 +0000

    Refactor environment variable access for bot token

--- send_reminder.py
+++ send_reminder.py
@@ -1,12 +1,12 @@
 import os
 import requests
 
-BOT_TOKEN = os.environ["8563381666:AAG6oWI5amPn0PQ274oh9iC47gNBU1DiAV0"]
-CHAT_ID = os.environ["CHAT_ID"]
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
-message = "🔔 Rappel BLS Espagne Tanger : vérifie les rendez-vous maintenant."
+message = "🔔 Rappel BLS Espagne Tanger"
 
-url = f"https://api.telegram.org/bot{8563381666:AAG6oWI5amPn0PQ274oh9iC47gNBU1DiAV0}/sendMessage"
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
 data = {
     "chat_id": CHAT_ID,
